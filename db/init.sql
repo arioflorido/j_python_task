ced015e737ebf7e9ffaedb6707722dcf461e907f
Aaron Rioflorido <20210994+aaronrx@users.noreply.github.com>
2021-12-08 19:40:53 +0800

    Updated database

--- db/init.sql
+++ db/init.sql
@@ -1,42 +1,37 @@
 CREATE DATABASE recipe_db;
 use recipe_db;
 
+CREATE TABLE recipes (
+  id int AUTO_INCREMENT,
+  title VARCHAR(80),
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE ingredients (
   id int AUTO_INCREMENT,
   name VARCHAR(40),
   best_before date,
   use_by date,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE recipes (
-  id int AUTO_INCREMENT,
-  title VARCHAR(80),
   PRIMARY KEY(id),
 
-  fk_ingredient_id int,
-  CONSTRAINT fk_ingredient_id
-  FOREIGN KEY (fk_ingredient_id)
-    REFERENCES ingredients(id)
+  recipe_id int,
+  FOREIGN KEY (recipe_id)
+    REFERENCES recipes(id)
 );
 
-INSERT INTO ingredients
-  (name, best_before, use_by)
-VALUES
-  ('Bacon', '2021-12-10', '2021-12-12'),
-  ('Eggs', '2021-12-11', '2021-12-13'),
-  ('Baked Beans', '2021-12-12', '2021-12-15'),
-  ('Mushroom', '2021-12-10', '2021-12-12'),
-  ('Sausage', '2021-12-10', '2021-12-12'),
-  ('Bread', '2021-12-10', '2021-12-12');
-
 INSERT INTO recipes
-  (title, fk_ingredient_id)
+  (title)
 VALUES
-  ('Ham and Cheese Sandwich', 1),
-  ('Ham and Cheese Sandwich', 2),
-  ('Ham and Cheese Sandwich', 3),
-  ('Ham and Cheese Sandwich', 4),
-  ('Ham and Cheese Sandwich', 5),
-  ('Ham and Cheese Sandwich', 6);
+  ('Ham and Cheese Sandwich');
 
+INSERT INTO ingredients
+  (name, best_before, use_by, recipe_id)
+VALUES
+  ('Bacon', '2021-12-10', '2021-12-12', 1),
+  ('Eggs', '2021-12-11', '2021-12-13', 1),
+  ('Baked Beans', '2021-12-12', '2021-12-15', 1),
+  ('Mushroom', '2021-12-10', '2021-12-12', 1),
+  ('Sausage', '2021-12-10', '2021-12-12', 1),
+  ('Bread', '2021-12-10', '2021-12-12', 1);
+
+-- TODO Create Shelf life table?
