e3c5caea72855cc45af107148cc6d0f47673bf39
Aaron Rioflorido <20210994+aaronrx@users.noreply.github.com>
2021-12-08 22:57:29 +0800

    Updated dbconn strings config

--- app/database.py
+++ app/database.py
@@ -3,17 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-dbtype = os.environ.get('dbtype', 'mysql+pymysql')
-user = os.environ.get('user', 'root')
-password = os.environ.get('password', 'root')
-host = os.environ.get('host', 'db')
-port = os.environ.get('port', 3306)
-db = os.environ.get('db', 'recipe_db')
+# Use default conn string for development purposes only.
+DATABASE_URL = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:root@localhost:3306/recipe_db')
 
-SQLALCHEMY_DATABASE_URI = f'{dbtype}://{user}:{password }@{host}:{port}/{db}'
-
-print(SQLALCHEMY_DATABASE_URI)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
