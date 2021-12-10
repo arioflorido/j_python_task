7362eeb1a00d8fb8ba3674558052d0f81bf75840
Aaron Rioflorido <20210994+aaronrx@users.noreply.github.com>
2021-12-11 00:09:00 +0800

    Cleanup

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers import router
-import uvicorn
-
-app = FastAPI()
-app.include_router(router)
-
-if __name__ == '__main__':
-    uvicorn.run(app, port=8001, host='0.0.0.0')
--- app/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class IngredientBase(BaseModel):
-    name: str
-
-class RecipeBase(BaseModel):
-    title: str
-    ingredients: list
