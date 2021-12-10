f2272327352bb08c3c22068828ac59c11b846c16
Aaron Rioflorido <20210994+aaronrx@users.noreply.github.com>
2021-12-11 00:07:52 +0800

    API versioning

--- web/app/api/v1/api.py
+++ web/app/api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import recipe, ingredient
 
+API_VERSION = "/api/v1"
+
 api_router = APIRouter()
-api_router.include_router(recipe.router)
-api_router.include_router(ingredient.router)
+api_router.include_router(recipe.router, prefix=API_VERSION)
+api_router.include_router(ingredient.router, prefix=API_VERSION)
