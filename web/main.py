20419c3e34f61c43f0aaacd8d06f191148904abc
Aaron Rioflorido <20210994+aaronrx@users.noreply.github.com>
2023-07-02 16:12:46 +0800

    Replaced requirements.txt with Pipfile

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from app.api.v1.api import api_router
 import uvicorn
 
@@ -6,4 +6,4 @@ app = FastAPI()
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8001, host='0.0.0.0', debug=True)
+    uvicorn.run(app, port=8001, host='0.0.0.0')
